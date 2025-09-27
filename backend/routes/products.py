b82ec4606a5ea2aeccdae2e289e822432ff1dfd5
Vedant005 <vedantkanekar909@gmail.com>
2025-09-27 15:31:05 +0530

    feat: offset-pagination

--- backend/routes/products.py
+++ backend/routes/products.py
@@ -4,12 +4,16 @@ from models import Product
 
 products_bp = Blueprint("products", __name__)
 
-
 @products_bp.route("/", methods=["GET"])
 def get_all_products():
     try:
-        products = Product.query.all()
-      
+       
+        page = request.args.get("page", 1, type=int)
+        limit = request.args.get("limit", 20, type=int)
+        offset = (page - 1) * limit
+
+        products = Product.query.offset(offset).limit(limit).all()
+
         products_list = []
         for p in products:
             products_list.append({
@@ -22,6 +26,20 @@ def get_all_products():
                 "discount_percentage": p.discount_percentage,
                 "actual_price": p.actual_price
             })
-        return jsonify({"success": True, "data": products_list}), 200
+
+        total_products = Product.query.count()
+        total_pages = (total_products + limit - 1) // limit
+
+        return jsonify({
+            "success": True,
+            "data": products_list,
+            "pagination": {
+                "page": page,
+                "limit": limit,
+                "total_pages": total_pages,
+                "total_products": total_products
+            }
+        }), 200
+
     except Exception as e:
         return jsonify({"success": False, "error": str(e)}), 500
