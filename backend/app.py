c33efc55cac1a0e38f35f0ca7fb8280366cadd84
Vedant005 <vedantkanekar909@gmail.com>
2025-10-02 22:41:04 +0530

    cors

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from extensions import db, migrate
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.config.from_object("config")
 
 db.init_app(app)
